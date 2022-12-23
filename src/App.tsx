e55bcaabbdbfede34ab293ced81181d65e6e7747
Dylan Beck <dylan@Dylans-MacBook-Pro.local>
2022-12-23 12:29:18 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Button } from '@mantine/core';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+  
+      </div>
+      <h1>Vite + React + BOOBS</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Make AP Physics Your Bitch!
+      </p>
+
+      <Button variant='light'>
+      I agree to sell my privacy
+      </Button>
+    </div>
+  )
+}
+
+export default App
